10c0fe996f7a7ef7b6fb3350e02621cc0a329c03
Taran Pala <36001700+Taran-Pala@users.noreply.github.com>
2025-06-03 13:49:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -20,27 +20,4 @@ function save(){
 
 function reset(){
     saveEl.textContent = "Previous entries: "
-=======
-
-let countEl = document.getElementById("count-el")
-let saveEl = document.getElementById("save-el")
-
-
-
-let count = 0
-
-function increment() {
-    count += 1
-    countEl.innerText = count
-}
-
-function save(){
-    saveEl.textContent += count + " - "
-    countEl.textContent = 0
-    count = 0
-}
-
-function reset(){
-    saveEl.textContent = "Previous entries: "
->>>>>>> master
 }
